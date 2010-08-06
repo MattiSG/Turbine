015b76e6ffb1dea46506f6ef23ab7b68dd22bfaf
Matti Schneider-Ghibaudo <alcmene.gs@gmail.com>
2010-08-06 15:38:49 +0200

    Modified config file for inclusion in PDC.

--- config.php
+++ config.php
@@ -16,11 +16,12 @@ $config = array(
 	// Mode 0 hides all error messages
 	// Mode 1 displays error messages related to the style sheets (like elements trying to inherit properties that don't exist)
 	// Mode 2 additionally displays php developer messages and sets error_reporting to E_ALL
-	'debug_level' => 2,
+	'debug_level' => (isset($_GET['debug_level']) ? $_GET['debug_level'] : 0),
 
 
 	// Base path to cssp and css files relative to css.php
-	'css_base_dir' => '',
+	// Must have the trailing slash!
+	'css_base_dir' => '../',
 
 
 	// Minify regular css files (true) oder include them completely unchanged (false)
